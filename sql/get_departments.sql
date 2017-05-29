96ffcd125b40f9321b036b77fcc04a8347be540e
Anton Ponomarev <aponomarev@docdoc.ru>
2017-05-29 16:15:20 +0800

    DD-7875 fix добавил обработку null значений в запросы

--- sql/get_departments.sql
+++ sql/get_departments.sql
@@ -1,9 +1,9 @@
 set nocount on;
 select 'id;parent_id;type;name;clinic_id;clinic_name;address;phone';
 select dpt.ID as id, dpt.ParentID as parent_id,
-DeptTypeID as type, Name as name, org.ID as clinic_id,
-org.FullName as clinic_name, org.PhysicalAddress as address,
-org.Phone as phone from Dept as dpt
+DeptTypeID as type, Name as name, isnull(org.ID, '') as clinic_id,
+isnull(org.FullName, '') as clinic_name, isnull(org.PhysicalAddress, '') as address,
+isnull(org.Phone, '') as phone from Dept as dpt
 left join Organisations as org
 on org.ID = dpt.OrgID
 where
